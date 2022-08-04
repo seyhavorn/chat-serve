90db0bc6836333b262e23201d33b317116694f0a
seyhavorn <vornseyha4758@gmail.com>
2022-08-04 14:02:53 +0700

    Create live chat

--- src/modules/messages/messages.gateway.ts
+++ src/modules/messages/messages.gateway.ts
@@ -1,15 +1,33 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+  ConnectedSocket,
+} from '@nestjs/websockets';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
 export class MessagesGateway {
+  @WebSocketServer()
+  serve: Server;
+
   constructor(private readonly messagesService: MessagesService) {}
 
   @SubscribeMessage('createMessage')
-  create(@MessageBody() createMessageDto: CreateMessageDto) {
-    return this.messagesService.create(createMessageDto);
+  async create(
+    @MessageBody() createMessageDto: CreateMessageDto,
+    @ConnectedSocket() client: Socket,
+  ) {
+    const message = this.messagesService.create(createMessageDto, client.id);
+    this.serve.emit('message', message);
+    return message;
   }
 
   @SubscribeMessage('findAllMessages')
@@ -17,18 +35,20 @@ export class MessagesGateway {
     return this.messagesService.findAll();
   }
 
-  @SubscribeMessage('findOneMessage')
-  findOne(@MessageBody() id: number) {
-    return this.messagesService.findOne(id);
+  @SubscribeMessage('join')
+  joinRoom(
+    @MessageBody('name') name: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    return this.messagesService.identify(name, client.id);
   }
 
-  @SubscribeMessage('updateMessage')
-  update(@MessageBody() updateMessageDto: UpdateMessageDto) {
-    return this.messagesService.update(updateMessageDto.id, updateMessageDto);
-  }
-
-  @SubscribeMessage('removeMessage')
-  remove(@MessageBody() id: number) {
-    return this.messagesService.remove(id);
+  @SubscribeMessage('typing')
+  async typing(
+    @MessageBody('isTyping') isTyping: boolean,
+    @ConnectedSocket() client: Socket,
+  ) {
+    const name = await this.messagesService.getClientName(client.id);
+    client.broadcast.emit('typing', { name, isTyping });
   }
 }
--- src/modules/messages/messages.service.ts
+++ src/modules/messages/messages.service.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
 
 @Injectable()
 export class MessagesService {
-  create(createMessageDto: CreateMessageDto) {
-    return 'This action adds a new message';
+  messages: Message[] = [{ name: 'Seyha Dev', text: 'Hello' }];
+  ClientToUser = {};
+
+  identify(name: string, clientId: string) {
+    this.ClientToUser[clientId] = name;
+
+    return Object.values(this.ClientToUser);
+  }
+
+  getClientName(clientId: string) {
+    return this.ClientToUser[clientId];
+  }
+
+  create(createMessageDto: CreateMessageDto, clientId: string) {
+    const message = {
+      name: this.ClientToUser[clientId],
+      text: createMessageDto.text,
+    };
+    this.messages.push(message);
+    return message;
   }
 
   findAll() {
-    return `This action returns all messages`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} message`;
-  }
-
-  update(id: number, updateMessageDto: UpdateMessageDto) {
-    return `This action updates a #${id} message`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} message`;
+    return this.messages;
   }
 }
